d41bc408620645f8b00803c1369b0119575d84fc
Ivan Zuev <ivan.zuev96@yandex.ru>
2022-12-29 18:50:04 +0300

    testng

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,27 +1,22 @@
 package ru.netology;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void testRemainNull() {
+    public void shouldReturnNull() {
         int actual = service.remain(1000);
         int expected = 0;
-
-        Assert.assertEquals(expected, actual);
-        //вводим сумму покупки 1000
-
-
+        Assert.assertEquals(actual, expected);
     }
-    @Test
-    public void testRemain300() {
-        int actual = service.remain(700);
-        int expected = 300;
 
-        Assert.assertEquals(expected, actual);
-        //вводим сумму покупки 300
+    @Test
+    public void shouldReturn300() {
+        int actual = service.remain(700);
+        int expected = 700;
+        Assert.assertEquals(actual, expected);
     }
 }
